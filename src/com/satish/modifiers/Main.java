92da46e7d23059dceb0968792d2711074f637f55
unknown <satishgurjar2020@gmail.com>
2025-09-15 21:33:08 +0530

    person class added

---
+++ src/com/satish/modifiers/Main.java
@@ -0,0 +1,18 @@
+package com.satish.modifiers;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Person p = new Person();
+		
+		p.showName();
+		
+		p.showCity();
+		
+	//	p.showAge();
+		
+		
+	}
+
+}
