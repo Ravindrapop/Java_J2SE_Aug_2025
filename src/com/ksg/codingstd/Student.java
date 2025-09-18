60e372ccd6f739939d29a3bf8ecb382e4e638aaf
komalahamana <komasinghgurjar43@gmail.com>
2025-09-18 07:24:01 +0530

    new code added for java bean code standard

---
+++ src/com/ksg/codingstd/Student.java
@@ -0,0 +1,26 @@
+package com.ksg.codingstd;
+
+public class Student {
+	
+	public int id;
+	public String name;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+
+
+
+}
