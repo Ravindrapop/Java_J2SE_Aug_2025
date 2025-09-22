be5bd1372b96a70f41502874530fb21ecd1083fa
Deepak Singh <deepak@example.com>
2025-09-22 10:38:47 +0530

    this is my decrement demo

---
+++ src/com/deepak/opretar/OpretarDemo2.java
@@ -0,0 +1,12 @@
+package com.deepak.opretar;
+
+public class OpretarDemo2 {
+
+	public static void main(String[] args) {
+		int x = 22;
+		int y = --x;
+		System.out.println(x + "  " + y);
+
+	}
+
+}
