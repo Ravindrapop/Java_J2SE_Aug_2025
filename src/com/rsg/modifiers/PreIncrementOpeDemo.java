82bc073dd49ff91ecde4a520020adf0e60a0300b
unknown <ravindra12329@gmail.com>
2025-09-22 06:40:07 +0530

    code added for preincrement operator demo

---
+++ src/com/rsg/modifiers/PreIncrementOpeDemo.java
@@ -0,0 +1,15 @@
+package com.rsg.modifiers;
+
+public class PreIncrementOpeDemo {
+
+	public static void main(String[] args) {
+		
+		int x = 4;
+		
+		int y = ++x;
+		
+		System.out.println(x+" "+y);
+
+	}
+
+}
