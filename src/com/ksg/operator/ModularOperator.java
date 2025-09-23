f91db4b184b5b38cc8e0bb6508d5d390ddb5f169
komalahamana <komasinghgurjar43@gmail.com>
2025-09-23 07:40:48 +0530

    code for operator

---
+++ src/com/ksg/operator/ModularOperator.java
@@ -0,0 +1,20 @@
+package com.ksg.operator;
+
+public class ModularOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(10%2);
+		
+		System.out.println(7%2);
+		
+		System.out.println(10/2);
+		
+		System.out.println(10+2);
+		
+		System.out.println(20-12);
+
+	}
+
+}
