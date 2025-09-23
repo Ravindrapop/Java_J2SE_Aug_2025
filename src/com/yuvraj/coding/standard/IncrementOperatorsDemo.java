d95665dfa377be7b5c21a377852dda902176e975
Yuvraj Singh Kushwah <kushwahyuvraj262@gmail.com>
2025-09-23 07:39:30 +0530

    pre post increment/decrement demo

---
+++ src/com/yuvraj/coding/standard/IncrementOperatorsDemo.java
@@ -0,0 +1,25 @@
+package com.yuvraj.coding.standard;
+
+public class IncrementOperatorsDemo {
+
+	public static void main(String[] args) {
+
+		// PreIncrement Operator Demo
+		int x = 10;
+		int y = ++x;
+ 
+		System.out.println(x+" and "+y);
+		System.out.println("final value of x = " + x);
+		System.out.println("final value of y = " + y);
+ 
+		// PostIncrement Operator Demo
+		int a = 5;
+		int b = a++;
+
+		System.out.println(a+" and "+b);
+		System.out.println("final value of a = " + a);
+		System.out.println("final value of b = " + b);
+
+	}
+
+}
