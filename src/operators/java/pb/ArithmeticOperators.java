82575395db94a29d05e4c7350536838e0390ff4f
unknown <poojabhadauria27@gmail.com>
2025-09-23 18:46:20 +0530

    I have commited Operator Class

---
+++ src/operators/java/pb/ArithmeticOperators.java
@@ -0,0 +1,17 @@
+package operators.java.pb;
+
+public class ArithmeticOperators {
+	public static void main(String [] args) {
+		
+		int p =60;int q =80;int r=90;
+		System.out.println(p+q-r);
+		System.out.println(p-q+r);
+		System.out.println(p%q*r);
+		System.out.println(p*q/r);
+		System.out.println(p/q*r);
+		System.out.println(p+q+r);
+		System.out.println(q-p-r);
+		
+	}
+
+}
---
+++ src/operators/java/pb/RelationalOperator.java
@@ -0,0 +1,17 @@
+package operators.java.pb;
+
+public class RelationalOperator {
+public static void main(String[] args) {
+	int a= 20;int b=35;int c=35;
+	
+	System.out.println(a==b); 
+	System.out.println(a>b);
+	System.out.println(a<=b);
+	System.out.println(a!=b);
+	System.out.println(a==c);
+	int p=45;int q=45;
+	System.out.println(p==q);
+	System.out.println(p!=q);
+}
+
+}
