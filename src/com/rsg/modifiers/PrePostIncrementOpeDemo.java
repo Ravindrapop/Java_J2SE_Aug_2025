efab2d9e7873b1c0fdf38e8431118e4d35d18c4f
unknown <ravindra12329@gmail.com>
2025-09-23 06:30:54 +0530

    pre post increment example code has been done

---
+++ src/com/rsg/modifiers/PrePostIncrementOpeDemo.java
@@ -0,0 +1,20 @@
+package com.rsg.modifiers;
+
+public class PrePostIncrementOpeDemo {
+
+	public static void main(String[] args) {
+		
+		int x = 4;
+		
+		int y = ++x;  
+		
+		System.out.println("x = "+x+" y = "+y);
+		
+		int a = 4;
+		int b = a++;
+		
+		System.out.println("a = "+a+" b = "+b);
+
+	}
+
+}
