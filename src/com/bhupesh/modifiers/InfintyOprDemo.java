a961b0d187ddbb70066a8470b680eb7147cfde87
Bhupesh Kumar <bhupeshagrahari.com>
2025-09-24 22:19:51 +0530

    arithmaticoprat and infintyopra

---
+++ src/com/bhupesh/modifiers/InfintyOprDemo.java
@@ -0,0 +1,19 @@
+package com.bhupesh.modifiers;
+
+public class InfintyOprDemo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(100/0.0);
+		
+		System.out.println(-100/0.0);
+		
+		System.out.println(0/0.0);
+		
+		System.out.println(0/0);
+		
+		System.out.println(-0/0.0);
+		
+	}
+
+}
