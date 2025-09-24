fea4b5891f588eaea260f00197849af557d681f1
Bhupesh Kumar <bhupeshagrahari.com>
2025-09-24 07:38:11 +0530

    incremrnt opr

---
+++ src/com/bhupesh/modifiers/DecrementDemo.java
@@ -0,0 +1,17 @@
+package com.bhupesh.modifiers;
+
+public class DecrementDemo {
+
+	public static void main(String[] args) {
+		
+		double d = 20.12;
+		
+		char ch = 'b';
+		
+		byte b = 10;
+		
+		System.out.println(d+ch+b);
+
+	}
+
+}
---
+++ src/com/bhupesh/modifiers/IncrementDemo.java
@@ -0,0 +1,20 @@
+package com.bhupesh.modifiers;
+
+public class IncrementDemo {
+
+	public static void main(String[] args) {
+		
+		int x = 8;
+		
+		int y = ++x;
+		
+	    System.out.println(x+y);
+	    
+	    System.out.println(x*y);
+	    
+	    System.out.println(x-y);
+	    
+
+	}
+
+}
