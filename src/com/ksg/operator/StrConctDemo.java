f30730d9bcac6d5fc7e13ab6ae8d56369a3b6724
komalahamana <komasinghgurjar43@gmail.com>
2025-09-24 07:14:51 +0530

    Relational codes are added

---
+++ src/com/ksg/operator/StrConctDemo.java
@@ -0,0 +1,21 @@
+package com.ksg.operator;
+
+public class StrConctDemo {
+
+	public static void main(String[] args) {
+		
+int a=10, b=20, c=30;
+		
+		String d = "Ankit";
+		
+		System.out.println(a+b+c+d); // 60Ankit
+		
+		System.out.println(a+b+d+c); //30Ankit30
+		
+		System.out.println(d+a+b+c); //Ankit102030
+		
+		System.out.println(a+d+b+c); // 10Ankit2030
+
+	}
+
+}
