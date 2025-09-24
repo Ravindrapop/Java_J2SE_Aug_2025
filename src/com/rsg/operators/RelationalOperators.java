e1df235e61cd8419a4412d32cf2295a5a44fc51b
vermaankit2003 <vermaankitk2003@gmail.com>
2025-09-24 23:20:28 +0530

    This is my Operators Demo file

---
+++ src/com/rsg/operators/RelationalOperators.java
@@ -0,0 +1,21 @@
+package com.rsg.operators;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		int a = 15;
+        int b = 20;
+        int c = 25;
+        int d = 30;
+
+        System.out.println("a == b is " + (a == b)); 
+        System.out.println("a != c is " + (a != c)); 
+        System.out.println("b > c is " + ( b > c));  
+        System.out.println("c < b is " + (c < b));  
+        System.out.println("c >= a is " + (c >= a)); 
+        System.out.println("c <= d is " + (c <= d)); 
+        
+	}
+	   }
+
+
