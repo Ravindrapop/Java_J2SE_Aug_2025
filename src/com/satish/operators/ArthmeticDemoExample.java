91fb6185bc6c6f0f65ef2cc96ade72d9b09421c0
unknown <satishgurjar2020@gmail.com>
2025-09-25 06:20:43 +0530

    code added arthmetic operator

---
+++ src/com/satish/operators/ArthmeticDemoExample.java
@@ -0,0 +1,13 @@
+package com.satish.operators;
+
+public class ArthmeticDemoExample {
+	public static void main(String[] args) {
+		double x = 10.5;
+		double y = 3.2;
+
+		System.out.println("Addition: " + (x + y));
+		System.out.println("Subtraction: " + (x - y));
+		System.out.println("Multiplication: " + (x * y));
+		System.out.println("Division: " + (x / y));
+	}
+}
