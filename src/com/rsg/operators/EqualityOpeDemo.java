66e30e85a534e0002f9187992db933cda445ec2f
unknown <ravindra12329@gmail.com>
2025-09-25 07:12:15 +0530

    examples equality operators have been added

---
+++ src/com/rsg/operators/EqualityOpeDemo.java
@@ -0,0 +1,15 @@
+package com.rsg.operators;
+
+public class EqualityOpeDemo {
+
+	public static void main(String[] args) {
+		//equality operator == and !=
+		
+		System.out.println(10==10.5);
+		System.out.println('b'==98);
+		System.out.println(true==false);
+		System.out.println(15!=15);
+
+	}
+
+}
