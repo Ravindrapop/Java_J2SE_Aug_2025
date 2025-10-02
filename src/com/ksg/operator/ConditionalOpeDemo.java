77333e0441722904b51feba99f7df330a1f17c91
komalahamana <komasinghgurjar43@gmail.com>
2025-10-02 08:08:00 +0530

    CondOpeDemo Code

---
+++ src/com/ksg/operator/ConditionalOpeDemo.java
@@ -0,0 +1,15 @@
+package com.ksg.operator;
+
+public class ConditionalOpeDemo {
+
+	public static void main(String[] args) {
+
+		int a = 22;
+		int b = 30;
+		
+		int x=(a>b) ? 40:50;
+		
+		System.out.println(x);
+	}
+
+}
