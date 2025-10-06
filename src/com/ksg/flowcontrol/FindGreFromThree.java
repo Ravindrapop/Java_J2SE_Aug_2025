bbc832a8241ed3e6e9e037b5418e6c96f77cb206
komalahamana <komasinghgurjar43@gmail.com>
2025-10-06 19:31:46 +0530

    codes to find greater

---
+++ src/com/ksg/flowcontrol/FindGreFromThree.java
@@ -0,0 +1,34 @@
+package com.ksg.flowcontrol;
+
+public class FindGreFromThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+
+		int b = 20;
+
+		int c = 100;
+
+
+		if(a>b){
+		        System.out.println("A is greater");
+		}else if(b>c){
+
+		        System.out.println("B is greater");
+
+		}else if(c>a){
+		    System.out.println("C is greater");
+
+
+		}else{
+
+		    System.out.println("A is greater");
+		}
+	}
+
+
+	
+
+}
