fe707aa5d221b0d1bb6e7540f6ec135a3bcc337e
Bhupesh Kumar <bhupeshagrahari.com>
2025-10-08 08:01:22 +0530

    this is my new commit

--- src/com/bhupesh/flowcontrol/FindEvenOrOdd.java
+++ src/com/bhupesh/flowcontrol/FindEvenOrOdd.java
@@ -4,14 +4,20 @@ public class FindEvenOrOdd {
 
 	public static void main(String[] args) {
 		
-		int a = 10;
+		int a = 11;
 		
-		if(a%5==0) {
+		int b = 21;
+		
+		if(a%2==0) {
 			System.out.println("Entered number is even");
-		}else {
+		}else if(b%2==0) {
 			System.out.println("Entered number is odd");
+		}else {
+			System.out.println("Entered both number is odd ");
+		}
+		
 		}
 
 	}
 
-}
+
