d70b497ba92702a9893534502d3a4f5bfba82c61
komalahamana <komasinghgurjar43@gmail.com>
2025-10-09 07:19:05 +0530

    sum and average for java8

---
+++ src/com/ksg/java8code/Java8SumAndAve.java
@@ -0,0 +1,20 @@
+package com.ksg.java8code;
+
+import java.util.Arrays;
+
+public class Java8SumAndAve {
+	
+	public static void main(String[] args) {
+		
+		int[] a = new int[] {45, 12, 56, 15, 24, 75, 31, 89};
+        
+        int sum = Arrays.stream(a).sum();
+         
+        System.out.println("Sum = "+sum);
+         
+        double average = Arrays.stream(a).average().getAsDouble();
+         
+        System.out.println("Average = "+average);
+	}
+
+}
