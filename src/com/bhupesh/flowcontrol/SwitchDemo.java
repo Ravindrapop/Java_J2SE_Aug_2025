65080f2e3814be8f4cefe45e3c85a6a168f3379f
Bhupesh Kumar <bhupeshagrahari.com>
2025-10-10 06:35:49 +0530

    src/com/bhupesh/flowcontrol/SwitchDemo.java

--- src/com/bhupesh/flowcontrol/SwitchDemo.java
+++ src/com/bhupesh/flowcontrol/SwitchDemo.java
@@ -4,45 +4,45 @@ public class SwitchDemo {
 	
 	public static void main(String[] args) {
 		
-		int a = 15;
+		int a = 11;
 		
 		switch (a) {
 		
 		case 1:
-			System.out.println("JANUARY");
+			System.out.println("This Month is JANUARY");
 			break;
 		case 2:
-			System.out.println("FEBRUARY");
+			System.out.println("This Month is FEBRUARY");
 			break;
 		case 3:
-			System.out.println("MARCH");
+			System.out.println("This Month is MARCH");
 			break;
 		case 4:
-			System.out.println("APRIL");
+			System.out.println("This Month is APRIL");
 			break;
 		case 5:
-			System.out.println("MAY");
+			System.out.println("This Month is MAY");
 			break;
 		case 6:
-			System.out.println("JUNE");
+			System.out.println("This Month is JUNE");
 			break;
 		case 7:
-			System.out.println("JULY");
+			System.out.println("This Month is JULY");
 			break;
 		case 8:
-			System.out.println("AUGUST");
+			System.out.println("This Month is AUGUST");
 			break;
 		case 9:
-			System.out.println("SEPTEMBER");
+			System.out.println("This Month is SEPTEMBER");
 			break;
 		case 10:
-			System.out.println("OCTOBER");
+			System.out.println("This Month is OCTOBER");
 			break;
 		case 11:
-			System.out.println("NOVEMBER");
+			System.out.println("This Month is NOVEMBER");
 			break;
 		case 12:
-			System.out.println("DECEMBER");
+			System.out.println("This Month is DECEMBER");
 			break;
 		default:
 			System.out.println("You have intered the wrong number.");
