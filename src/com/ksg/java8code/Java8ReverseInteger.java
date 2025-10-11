3d15bd6410111a8e04ae442acfc2e229e253dac7
komalahamana <komasinghgurjar43@gmail.com>
2025-10-11 21:04:54 +0530

    java 8 codes

---
+++ src/com/ksg/java8code/Java8ReverseInteger.java
@@ -0,0 +1,19 @@
+package com.ksg.java8code;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Java8ReverseInteger {
+	
+	public static void main(String[] args) {
+		
+
+	    {
+	        int[] array = new int[] {5, 1, 7, 3, 9, 6};
+	         
+	        int[] reversedArray = IntStream.rangeClosed(1, array.length).map(i -> array[array.length - i]).toArray();
+	         
+	        System.out.println(Arrays.toString(reversedArray));
+	    }
+	}
+}
