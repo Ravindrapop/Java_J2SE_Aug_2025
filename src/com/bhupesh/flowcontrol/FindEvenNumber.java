595818f3162a419ba0416938d59458f32df555da
Bhupesh Kumar <bhupeshagrahari.com>
2025-10-14 21:42:34 +0530

    this is my while loop number

---
+++ src/com/bhupesh/flowcontrol/FindEvenNumber.java
@@ -0,0 +1,16 @@
+package com.bhupesh.flowcontrol;
+
+public class FindEvenNumber {
+	
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while(i<=50) {
+			if(i%3==0)
+				System.out.println(i);
+			i++;
+		}
+	}
+
+}
---
+++ src/com/bhupesh/flowcontrol/WhileDemo.java
@@ -0,0 +1,17 @@
+package com.bhupesh.flowcontrol;
+
+public class WhileDemo {
+	
+	public static void main(String[] args) {
+		
+		int i = 5;
+		
+		while (i<=50) {
+			i++;
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
