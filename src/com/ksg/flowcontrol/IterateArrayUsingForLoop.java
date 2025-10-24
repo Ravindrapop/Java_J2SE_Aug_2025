21bcb4ba4e01870f1911e12a407a94d505ca0d4b
komalahamana <komasinghgurjar43@gmail.com>
2025-10-24 19:03:41 +0530

    for loop

---
+++ src/com/ksg/flowcontrol/IterateArrayUsingForLoop.java
@@ -0,0 +1,18 @@
+package com.ksg.flowcontrol;
+
+public class IterateArrayUsingForLoop {
+	
+	public static void main(String[] args) {
+		
+		char[] vowels = {'a','e','i','o','u'};
+		
+		for(int i = 0; i<vowels.length; i++) {
+			
+			System.out.println(vowels[i]);
+		}
+		
+		
+		
+	}
+
+}
