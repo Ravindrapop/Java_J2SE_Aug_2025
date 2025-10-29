ee9d3275df419d6d21f74f4fc1ba673a55eb20d1
sangamchauhan16 <kavvigurjar222@gamil.com>
2025-10-29 08:10:28 +0530

    added my code of forloop problem solving

---
+++ src1/com/sg/flowcontrol/ForLoop1.java
@@ -0,0 +1,14 @@
+package com.sg.flowcontrol;
+
+public class ForLoop1 {
+
+	public static void main(String[] args) {
+		 
+		for(int i = 18 ; i  <= 180; i+=18) {
+			
+			System.out.println( i );
+		}
+
+	}
+
+}
--- src1/com/sg/operators/EqualityOpeDemo.java
+++ src1/com/sg/operators/EqualityOpeDemo.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 
 		System.out.println('c'== 98);
 		
-		System.out.println(101!=11);
+		System.out.println(11!=11);
 	}
 
 }
